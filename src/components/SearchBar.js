39ad5aedae95b91c32cff2db8bb47f8d4a116f97
JalenMuller <49196998+JalenMuller@users.noreply.github.com>
2021-10-05 22:22:21 +0200

    Translations added
    Added translations for the placeholder text in the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,14 @@
 import { GeoFill } from 'react-bootstrap-icons';
 import {useState} from 'react'
+import Translate from '../functions/Translate';
 
 function SearchBar(props){
     const [showLocationDiv, setLocationDiv] = useState(false)
-    
+    const langId = localStorage.getItem('langId')
+    const placeholderText = {
+        nl: 'Zoek naar jouw stad',
+        en: 'Search for your city'
+    }
     const search = (e) => {
         
         if (e.charCode === 13) {
@@ -16,14 +21,14 @@ function SearchBar(props){
     return(
         <div className="search-content">
         <input type="text"
-               placeholder="Search by city"
+               placeholder= {placeholderText[langId]}
                onKeyPress={search}
                onFocus={() => setLocationDiv(true)}
                onBlur={() => setLocationDiv(false)}
                className="search-input"
         />
         {showLocationDiv &&
-        <div className="use-location" onMouseDown={props.getLocalWeather}><GeoFill color="#ff4800" size={24} className="geo-fill"/>Or use your location</div>
+        <div className="use-location" onMouseDown={props.getLocalWeather}><GeoFill color="#ff4800" size={24} className="geo-fill"/><Translate string="use-location" defaultString="Or use your location"/></div>
         }
         {/* <button className="search-button" onClick={getWeather}><img src={searchIcon}/></button> */}
         {/* <button className="search-button" onClick={getLocalWeather}><img className="small-icon" src={locationIcon}/></button> */}
